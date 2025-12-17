1a7c147f1dcdbd2cc6f775997a65cacfdd89e0d9
Misbah <misbahtu2@gmail.com>
2025-12-17 09:37:45 +0530

    Add operator, equal and clear buttons

--- Calculator.java
+++ Calculator.java
@@ -4,80 +4,94 @@ import javax.swing.border.LineBorder;
 
 public class Calculator {
 
-    JFrame jf;
-    JLabel DisplayLabel;
-    
-    JButton oneButton, twoButton, threeButton, fourButton, fiveButton;
-    JButton sixButton, sevenButton, eightButton, nineButton, zeroButton;
+	JFrame jf;
+	JLabel DisplayLabel;
+
+	JButton oneButton, twoButton, threeButton, fourButton, fiveButton;
+	JButton sixButton, sevenButton, eightButton, nineButton, zeroButton;
+	JButton plusButton, minusButton, mulButton, divButton;
+	JButton equalButton, dotButton, clearButton;
 
 
-    public Calculator() {
-        jf = new JFrame("Calculator");
-        jf.setLayout(null);
-        jf.setSize(550, 600);
-        jf.setLocation(450, 100);
+	public Calculator() {
+		jf = new JFrame("Calculator");
+		jf.setLayout(null);
+		jf.setSize(550, 600);
+		jf.setLocation(450, 100);
 
-        DisplayLabel = new JLabel("");
-        DisplayLabel.setBounds(30, 50, 480, 50);
-        DisplayLabel.setFont(new Font("Serif", Font.PLAIN, 40));
-        DisplayLabel.setBorder(new LineBorder(Color.BLACK, 2));
-        DisplayLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		DisplayLabel = new JLabel("");
+		DisplayLabel.setBounds(30, 50, 480, 50);
+		DisplayLabel.setFont(new Font("Serif", Font.PLAIN, 40));
+		DisplayLabel.setBorder(new LineBorder(Color.BLACK, 2));
+		DisplayLabel.setHorizontalAlignment(SwingConstants.RIGHT);
 
-        jf.add(DisplayLabel);
-        jf.setVisible(true);
-        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        sevenButton = new JButton("7");
-        sevenButton.setBounds(30,130,80,80);
-        sevenButton.setFont(new Font("Arial", Font.PLAIN, 40));
-        jf.add(sevenButton);
+		jf.add(DisplayLabel);
+		jf.setVisible(true);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        eightButton = new JButton("8");
-        eightButton.setBounds(130,130,80,80);
-        eightButton.setFont(new Font("Arial", Font.PLAIN, 40));
-        jf.add(eightButton);
+		sevenButton = new JButton("7");
+		sevenButton.setBounds(30, 130, 80, 80);
+		sevenButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		jf.add(sevenButton);
 
-        nineButton = new JButton("9");
-        nineButton.setBounds(230,130,80,80);
-        nineButton.setFont(new Font("Arial", Font.PLAIN, 40));
-        jf.add(nineButton);
-        
-        fourButton=new JButton("4");
-		fourButton.setBounds(30,230,80,80);
+		eightButton = new JButton("8");
+		eightButton.setBounds(130, 130, 80, 80);
+		eightButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		jf.add(eightButton);
+
+		nineButton = new JButton("9");
+		nineButton.setBounds(230, 130, 80, 80);
+		nineButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		jf.add(nineButton);
+
+		fourButton = new JButton("4");
+		fourButton.setBounds(30, 230, 80, 80);
 		fourButton.setFont(new Font("Arial", Font.PLAIN, 40));
 		jf.add(fourButton);
-		
-		 fiveButton=new JButton("5");
-		fiveButton.setBounds(130,230,80,80);
+
+		fiveButton = new JButton("5");
+		fiveButton.setBounds(130, 230, 80, 80);
 		fiveButton.setFont(new Font("Arial", Font.PLAIN, 40));
 		jf.add(fiveButton);
-		
-		sixButton=new JButton("6");
-		sixButton.setBounds(230,230,80,80);
+
+		sixButton = new JButton("6");
+		sixButton.setBounds(230, 230, 80, 80);
 		sixButton.setFont(new Font("Arial", Font.PLAIN, 40));
 		jf.add(sixButton);
-		
-		 oneButton=new JButton("1");
-		oneButton.setBounds(30,330,80,80);
+
+		oneButton = new JButton("1");
+		oneButton.setBounds(30, 330, 80, 80);
 		oneButton.setFont(new Font("Arial", Font.PLAIN, 40));
 		jf.add(oneButton);
-		
-          twoButton=new JButton("2");
-		twoButton.setBounds(130,330,80,80);
+
+		twoButton = new JButton("2");
+		twoButton.setBounds(130, 330, 80, 80);
 		twoButton.setFont(new Font("Arial", Font.PLAIN, 40));
 		jf.add(twoButton);
-		
-		 threeButton=new JButton("3");
-		 threeButton.setBounds(230,330,80,80);
-		 threeButton.setFont(new Font("Arial", Font.PLAIN, 40));
+
+		threeButton = new JButton("3");
+		threeButton.setBounds(230, 330, 80, 80);
+		threeButton.setFont(new Font("Arial", Font.PLAIN, 40));
 		jf.add(threeButton);
+		
+		plusButton = new JButton("+");
+		plusButton.setBounds(430,130,80,280);
+		plusButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		jf.add(plusButton);
 
-        
-    }
+		equalButton = new JButton("=");
+		equalButton.setBounds(230,430,180,80);
+		equalButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		jf.add(equalButton);
 
-    
-    
-    public static void main(String[] args) {
-        new Calculator();
-    }
+		clearButton = new JButton("Clear");
+		clearButton.setBounds(430,430,80,80);
+		jf.add(clearButton);
+
+
+	}
+
+	public static void main(String[] args) {
+		new Calculator();
+	}
 }
