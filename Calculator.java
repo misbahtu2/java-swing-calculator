7e125315baed06576c7ab8da884867752c150f52
Misbah <misbahtu2@gmail.com>
2025-12-17 09:35:00 +0530

    Add numeric buttons with layout  6-0

--- Calculator.java
+++ Calculator.java
@@ -41,6 +41,36 @@ public class Calculator {
         nineButton.setBounds(230,130,80,80);
         nineButton.setFont(new Font("Arial", Font.PLAIN, 40));
         jf.add(nineButton);
+        
+        fourButton=new JButton("4");
+		fourButton.setBounds(30,230,80,80);
+		fourButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		jf.add(fourButton);
+		
+		 fiveButton=new JButton("5");
+		fiveButton.setBounds(130,230,80,80);
+		fiveButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		jf.add(fiveButton);
+		
+		sixButton=new JButton("6");
+		sixButton.setBounds(230,230,80,80);
+		sixButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		jf.add(sixButton);
+		
+		 oneButton=new JButton("1");
+		oneButton.setBounds(30,330,80,80);
+		oneButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		jf.add(oneButton);
+		
+          twoButton=new JButton("2");
+		twoButton.setBounds(130,330,80,80);
+		twoButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		jf.add(twoButton);
+		
+		 threeButton=new JButton("3");
+		 threeButton.setBounds(230,330,80,80);
+		 threeButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		jf.add(threeButton);
 
         
     }
